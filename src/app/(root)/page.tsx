ed4986ce2e2023cdbc39f6cd52ec64c2df202c8e
IamShihabPro <mshihab.pro@gmail.com>
2023-08-11 17:31:47 +0600

    category card

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,10 +1,14 @@
 import Banner from "@/components/home/Banner";
+import JobSection from "@/components/home/JobSection";
 import PopularServices from "@/components/home/services/PopularServices";
 
 const Home = () => {
   return (
     <main>
       <Banner />
+
+      <JobSection></JobSection>
+      
       <PopularServices />
     </main>
   );
--- src/constant/Constant.ts
+++ src/constant/Constant.ts
@@ -89,3 +89,121 @@ export const popularServicesSliderContent = [
     image: "https://swiperjs.com/demos/images/nature-4.jpg",
   },
 ];
+
+
+export const jobCategory = {
+  title: "Service By Category",
+  description:
+    "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Exercitationem est error natus quidem velit recusandae a. Debitis deserunt sit quidem?",
+};
+
+export const Jobs =[
+  {
+    "name": "Alex Hales",
+    "username": "alex123",
+    "profile_picture": "https://example.com/profile/johndoe123.jpg",
+    "cover_photo": "https://img.freepik.com/free-vector/technology-landing-page-with-smartphone_23-2148266828.jpg?w=996&t=st=1691727399~exp=1691727999~hmac=7f6ee8cb0cde06134fd5ce239aa81c54af9956b38c894857ae7d70cf7659a468",
+    "work_category": "Big Data Analysis",
+    "reviews": [
+      {
+        "reviewer": "Jane Smith",
+        "comment": "John did an amazing job on my project. His creativity and attention to detail are remarkable.",
+        "rating": 4.8
+      },
+      {
+        "reviewer": "Mike Johnson",
+        "comment": "I was impressed by John's professionalism and quick turnaround. Definitely recommend!",
+        "rating": 4.6
+      },
+      {
+        "reviewer": "Emily Brown",
+        "comment": "It was a pleasure working with John. He understood my requirements perfectly.",
+        "rating": 5.0
+      }
+    ],
+    "ratings": 4.8,
+    "price": 100
+  },
+  
+  {
+    "name": "John Doe",
+    "username": "johndoe123",
+    "profile_picture": "https://example.com/profile/johndoe123.jpg",
+    "cover_photo": "https://img.freepik.com/free-vector/artificial-intelligence-concept-webinar_23-2150376953.jpg?w=996&t=st=1691727539~exp=1691728139~hmac=788b3abd676cde026c9f5dd21129068716feb6c21686084c7c8bad1b164ddca6",
+    "work_category": "Data Scientist",
+    "reviews": [
+      {
+        "reviewer": "Jane Smith",
+        "comment": "John did an amazing job on my project. His creativity and attention to detail are remarkable.",
+        "rating": 4.8
+      },
+      {
+        "reviewer": "Mike Johnson",
+        "comment": "I was impressed by John's professionalism and quick turnaround. Definitely recommend!",
+        "rating": 4.6
+      },
+      {
+        "reviewer": "Emily Brown",
+        "comment": "It was a pleasure working with John. He understood my requirements perfectly.",
+        "rating": 5.0
+      }
+    ],
+    "ratings": 4.8,
+    "price": 200
+  },
+
+  {
+    "name": "John Cena",
+    "username": "johncena123",
+    "profile_picture": "https://example.com/profile/johndoe123.jpg",
+    "cover_photo": "https://img.freepik.com/free-psd/accountant-concept-banner-template_23-2148626701.jpg?w=1060&t=st=1691727623~exp=1691728223~hmac=edc1991b60c55478caff97e66ee31e7fb597cdbf18cf1e168f8023daaa3228ba",
+    "work_category": "Web Developer",
+    "reviews": [
+      {
+        "reviewer": "Jane Smith",
+        "comment": "John did an amazing job on my project. His creativity and attention to detail are remarkable.",
+        "rating": 4.8
+      },
+      {
+        "reviewer": "Mike Johnson",
+        "comment": "I was impressed by John's professionalism and quick turnaround. Definitely recommend!",
+        "rating": 4.6
+      },
+      {
+        "reviewer": "Emily Brown",
+        "comment": "It was a pleasure working with John. He understood my requirements perfectly.",
+        "rating": 5.0
+      }
+    ],
+    "ratings": 4.8,
+    "price": 500
+  },
+  
+  {
+    "name": "Virat Kolly",
+    "username": "johncena123",
+    "profile_picture": "https://example.com/profile/johndoe123.jpg",
+    "cover_photo": "https://img.freepik.com/free-psd/graphic-design-banner-template_23-2149008233.jpg?w=1060&t=st=1691727712~exp=1691728312~hmac=de75407f11cca5807c03abdbc10e5d61f87b37dae1454ec406f2c5cab38cfc36",
+    "work_category": "Graphics Designer",
+    "reviews": [
+      {
+        "reviewer": "Jane Smith",
+        "comment": "John did an amazing job on my project. His creativity and attention to detail are remarkable.",
+        "rating": 4.8
+      },
+      {
+        "reviewer": "Mike Johnson",
+        "comment": "I was impressed by John's professionalism and quick turnaround. Definitely recommend!",
+        "rating": 4.6
+      },
+      {
+        "reviewer": "Emily Brown",
+        "comment": "It was a pleasure working with John. He understood my requirements perfectly.",
+        "rating": 5.0
+      }
+    ],
+    "ratings": 4.8,
+    "price": 550
+  }
+  
+]
