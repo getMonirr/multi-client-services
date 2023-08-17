13b4ebed384e9a28a4fed2f45741e70f42d7d5c9
Md. Monir Hoshen <getmonirr@gmail.com>
2023-08-17 22:46:27 +0600

    firese base fix mehetaj

--- firebase/firebase.config.ts
+++ firebase/firebase.config.ts
@@ -1,10 +1,15 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { ApiKey, AppId, AuthDomain, MessagingSenderId, ProjectId, StorageBucket } from "../env";
+// Import the functions you need from the SDKs you need
+import {
+  ApiKey,
+  AppId,
+  AuthDomain,
+  MessagingSenderId,
+  ProjectId,
+  StorageBucket,
+} from "../env";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-
-
 
 // Your web app's Firebase configuration
 console.log(process.env);
@@ -13,12 +18,12 @@ const firebaseConfig = {
   authDomain: AuthDomain,
   projectId: ProjectId,
   storageBucket: StorageBucket,
-  messagingSenderId:MessagingSenderId,
-  appId: AppId
+  messagingSenderId: MessagingSenderId,
+  appId: AppId,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 // loadEnvConfig(app)
 
-export default app
+export default app;
