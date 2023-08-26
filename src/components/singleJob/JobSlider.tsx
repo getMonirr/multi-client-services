8f0dcefc09711f18e4f5f13424c8b4fa1c7f7d6b
Md. Monir Hoshen <getmonirr@gmail.com>
2023-08-26 20:29:05 +0600

    single services added

---
+++ src/components/singleJob/JobSlider.tsx
@@ -0,0 +1,140 @@
+"use client";
+import React, { useRef, useState } from "react";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/free-mode";
+import "swiper/css/navigation";
+import "swiper/css/thumbs";
+
+// import required modules
+import { FreeMode, Navigation, Thumbs } from "swiper/modules";
+
+const JobSlider = () => {
+  const [thumbsSwiper, setThumbsSwiper] = useState<any>(null);
+
+  return (
+    <>
+      <Swiper
+        style={
+          {
+            "--swiper-navigation-color": "#fff",
+            "--swiper-pagination-color": "#fff",
+          } as any
+        }
+        loop={true}
+        spaceBetween={10}
+        navigation={true}
+        thumbs={{ swiper: thumbsSwiper }}
+        modules={[FreeMode, Navigation, Thumbs]}
+        className="h-[400px]"
+      >
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-1.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-2.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-3.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-4.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-5.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-6.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-7.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-8.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-9.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-10.jpg" />
+        </SwiperSlide>
+      </Swiper>
+      <Swiper
+        onSwiper={setThumbsSwiper}
+        loop={true}
+        spaceBetween={10}
+        slidesPerView={4}
+        freeMode={true}
+        watchSlidesProgress={true}
+        modules={[FreeMode, Navigation, Thumbs]}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+          <img
+            className="h-[100px] w-full object-cover object-center"
+            src="https://swiperjs.com/demos/images/nature-1.jpg"
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img
+            className="h-[100px] w-full object-cover object-center"
+            src="https://swiperjs.com/demos/images/nature-2.jpg"
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img
+            className="h-[100px] w-full object-cover object-center"
+            src="https://swiperjs.com/demos/images/nature-3.jpg"
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img
+            className="h-[100px] w-full object-cover object-center"
+            src="https://swiperjs.com/demos/images/nature-4.jpg"
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img
+            className="h-[100px] w-full object-cover object-center"
+            src="https://swiperjs.com/demos/images/nature-5.jpg"
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img
+            className="h-[100px] w-full object-cover object-center"
+            src="https://swiperjs.com/demos/images/nature-6.jpg"
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img
+            className="h-[100px] w-full object-cover object-center"
+            src="https://swiperjs.com/demos/images/nature-7.jpg"
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img
+            className="h-[100px] w-full object-cover object-center"
+            src="https://swiperjs.com/demos/images/nature-8.jpg"
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img
+            className="h-[100px] w-full object-cover object-center"
+            src="https://swiperjs.com/demos/images/nature-9.jpg"
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img
+            className="h-[100px] w-full object-cover object-center"
+            src="https://swiperjs.com/demos/images/nature-10.jpg"
+          />
+        </SwiperSlide>
+      </Swiper>
+    </>
+  );
+};
+
+export default JobSlider;
