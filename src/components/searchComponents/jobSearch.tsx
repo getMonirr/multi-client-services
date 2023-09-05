1af8b0e53e5c0a77e478f945c6f0160a904e7786
Sagor <sagor.heaking@gmail.com>
2023-09-05 21:00:58 +0600

    not a change

--- src/components/searchComponents/jobSearch.tsx
+++ src/components/searchComponents/jobSearch.tsx
@@ -11,35 +11,30 @@ import "swiper/css";
 
 import Image from "next/image";
 
-
 const SearchJobs = () => {
-
-  const [pageData, setPageData] = useState<any>([])
+  const [pageData, setPageData] = useState<any>([]);
   // if(findJobs.length >= 10){
   //   setPageData(findJobs)
   // }
   const [tabIndex, setTabIndex] = useState<number>(0);
-  const [currentPage, setCurrentPage] = useState<number>(0)
-  const totalItems: number = findJobs.length 
-  const perPage : number = 10 
-  const totalPage : number = Math.ceil(totalItems/ perPage)
-  const pageNumber : any = [...Array(totalPage).keys()]
-  
-  const pageHandle = (pages:number) =>{
-    setCurrentPage(pages)
-    const backData = (currentPage * perPage)
-    const currentData = findJobs.splice(backData, perPage)
-    setPageData(currentData)
+  const [currentPage, setCurrentPage] = useState<number>(0);
+  const totalItems: number = findJobs.length;
+  const perPage: number = 10;
+  const totalPage: number = Math.ceil(totalItems / perPage);
+  const pageNumber: any = [...Array(20).keys()];
 
+  const pageHandle = (page: number) => {
+    setCurrentPage(page);
+    const backData = currentPage * perPage;
+    const currentData = findJobs.splice(backData, perPage);
+    setPageData(currentData);
+  };
 
-  }
-
-  useEffect(()=>{
-    const fastData = findJobs.splice(0 , perPage)
-    setPageData(fastData)
-  },[])
-  console.log(findJobs)
-
+  useEffect(() => {
+    const fastData = findJobs.splice(0, perPage);
+    setPageData(fastData);
+  }, []);
+  console.log(findJobs);
 
   return (
     <div>
@@ -53,35 +48,31 @@ const SearchJobs = () => {
         <p className="p-4"> {findJobs.length} jobs found</p>
       </div>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 mx-auto">
-        {pageData.map((job: any, i:number) => (
+        {pageData.map((job: any, i: number) => (
           <div
             key={i}
             className="bg-white rounded overflow-hidden group shadow-md"
           >
             <Swiper
-            navigation={true}
-            modules={[Navigation]}
-            spaceBetween={20}
-            className=" "
-             >
-              {
-                job.picture.map((image:any , index:number ) =>
-                <SwiperSlide
-                key={i}
-                >
+              navigation={true}
+              modules={[Navigation]}
+              spaceBetween={20}
+              className=" "
+            >
+              {job.picture.map((image: any, index: number) => (
+                <SwiperSlide key={i}>
                   <div className="w-full h-52 border-b">
-                  <Image
-                  src={image}
-                  alt="Vercel Logo"
-                  className=""
-                  width={300}
-                  height={208}
-                  priority
-                />
+                    <Image
+                      src={image}
+                      alt="Vercel Logo"
+                      className=""
+                      width={300}
+                      height={208}
+                      priority
+                    />
                   </div>
-                </SwiperSlide>)
-              }
-              
+                </SwiperSlide>
+              ))}
             </Swiper>
 
             <div className="pt-4 pb-3 px-4">
@@ -109,16 +100,24 @@ const SearchJobs = () => {
       </div>
 
       <div className="mt-10 text-center">
-        <p>Current Page {currentPage} </p>
-      {
-        pageNumber.map((page:number) => <button
-         className={currentPage === page ? "bg-white rounded-full p-2" : "rounded-full p-2 bg-blue-400 ml-2"}
-         key={page}
-         onChange={()=>pageHandle(page)}
-         >
-           {page} 
-         </button>)
-      }
+        <div className="">
+          <button className="join-item btn">«</button>
+          <button className="join-item btn">Page{currentPage}</button>
+          <button className="join-item btn">»</button>
+        </div>
+        {pageNumber.map((page: number) => (
+          <button
+            className={
+              currentPage === page
+                ? "bg-white rounded-full p-2"
+                : "rounded-full p-2 bg-blue-400 ml-2"
+            }
+            key={page}
+            onClick={() => pageHandle(page)}
+          >
+            {page}
+          </button>
+        ))}
       </div>
     </div>
   );
