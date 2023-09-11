eeb8099e0dfb5c0b29f6a9f7321af544e0ec566e
Mehetaj Khandaker <121656128+Mehetaj@users.noreply.github.com>
2023-09-11 21:27:19 +0600

    Revert "many page responsive"

--- src/Context/AuthProvider.tsx
+++ src/Context/AuthProvider.tsx
@@ -3,6 +3,7 @@ import React, { createContext, useEffect, useState } from "react";
 import {
   GithubAuthProvider,
   GoogleAuthProvider,
+  NextOrObserver,
   User,
   createUserWithEmailAndPassword,
   getAuth,
@@ -21,35 +22,41 @@ type Props = {
 };
 
 export const AuthContext = createContext(null);
-const auth = getAuth(app);
+const auth: any = getAuth(app);
 
 const GoogleProvider = new GoogleAuthProvider();
 const GitHubProvider = new GithubAuthProvider();
 
 const AuthProvider = ({ children }: Props) => {
-  const [user, setUser] = useState<User | null | undefined>();
+  const [user, setUser] = useState<User>();
   const [loading, setLoading] = useState<boolean>(true);
 
-  const createUser = (email: string, password: string) => {
+  const createUser: object = (email: string, password: string) => {
     setLoading(true);
     return createUserWithEmailAndPassword(auth, email, password);
   };
 
-  const signIn = (email: string, password: string) => {
+  const signIn: any = (email: string, password: string) => {
     setLoading(true);
     return signInWithEmailAndPassword(auth, email, password);
   };
 
-  const googleSignIn = () => {
+  const googleSignIn: any = () => {
     setLoading(true);
     return signInWithPopup(auth, GoogleProvider);
   };
-  const githubSignIn = () => {
+  const githubSignIn: any = () => {
     setLoading(true);
     return signInWithPopup(auth, GitHubProvider);
   };
 
-  
+  const updateUserProfile: any = (name: string, picture: string) => {
+    return updateProfile(auth.currentUser, {
+      displayName: name,
+      photoURL: picture,
+    });
+  };
+
   const logOut: any = () => {
     setLoading(true);
     return signOut(auth);
@@ -77,10 +84,11 @@ const AuthProvider = ({ children }: Props) => {
     signIn,
     googleSignIn,
     logOut,
+    updateUserProfile,
     githubSignIn,
     reset_password,
   };
-  return <AuthContext.Provider value={values}>{children}</AuthContext.Provider>
+  return <AuthContext.Provider value={values}>{children}</AuthContext.Provider>;
 };
 
 export default AuthProvider;
