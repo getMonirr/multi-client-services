1779b91343f5d97ddb0ab230fe1b5415760c55ac
Mehetaj Khandaker <mehetajkhandaker@gmail.com>
2023-09-12 22:37:32 +0600

    collect form data in create service page

--- src/app/dashboard/create_my_service/publish/page.tsx
+++ src/app/dashboard/create_my_service/publish/page.tsx
@@ -1,19 +1,22 @@
 import SimpleBtn from '@/components/shared/btn/SimpleBtn';
 import React from 'react';
 
-const Publish_service = () => {
+const Publish_service = ({ publish_data }: any) => {
+    
     return (
-       <div className='flex justify-center items-center'>
-         <div>
-        <h1 className='text-4xl text-center font-bold my-6'>Almost There....</h1>
-        <p className='my-4 text-[20px] text-center text-gray-500'>Let's Publish your service and <br />
-        Get some buyer in rolling
-        </p>
-            <SimpleBtn className='w-[500px] text-xl'>
-                Publish Your Service
-            </SimpleBtn>
+        <div className='flex justify-center items-center'>
+            <div>
+                <h1 className='text-4xl text-center font-bold my-6'>Almost There....</h1>
+                <p className='my-4 text-[20px] text-center text-gray-500'>Let's Publish your service and <br />
+                    Get some buyer in rolling
+                </p>
+                <button onClick={publish_data}>
+                    
+                        Publish Your Service
+                    
+                </button>
+            </div>
         </div>
-       </div>
     );
 };
 
