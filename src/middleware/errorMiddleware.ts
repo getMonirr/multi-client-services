5197d2471d2722c32b54294f84939c8a9c5f94d2
Md. Monir Hoshen <getmonirr@gmail.com>
2023-09-23 19:34:39 +0600

    user email query done

--- src/middleware/errorMiddleware.ts
+++ src/middleware/errorMiddleware.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
 
-export const handleError = (error: Error) => {
-  return NextResponse.json({ error: error.message });
+export const handleError = (error: any) => {
+  return NextResponse.json(error);
 };
